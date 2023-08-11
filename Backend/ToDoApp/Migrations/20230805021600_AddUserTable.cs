062068c22ebf6a95231da3ddd297a23fb5fbbd29
DJSivick <djsivick@gmail.com>
2023-08-11 01:23:35 -0400

    Change file structure for backend code

---
+++ Backend/ToDoApp/Migrations/20230805021600_AddUserTable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.email);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
---
+++ Backend/ToDoApp/Models/ToDo.cs
@@ -0,0 +1,18 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDo
+    {   
+        public string id { get; set; }
+        public string user_email { get; set; }
+
+        public string title { get; set; }
+
+        public string description { get; set; }
+
+        public DateTime date { get; set; }
+
+        public int progress { get; set; }
+
+        public int status { get; set; }
+    }
+}
